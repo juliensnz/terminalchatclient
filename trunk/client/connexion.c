65cbad04df764c141dab07de5081ffced5549247
sumbobyboys <sumbobyboys@e84e6728-c4a8-11de-8f3f-2f730f80caef>
2009-11-05 14:52:00 +0000

    Ajout du fichier de structure

--- trunk/client/connexion.c
+++ trunk/client/connexion.c
@@ -1,9 +1,11 @@
 /*
- *  structure.h
+ *  connexion.c
  *  client
  *
  *  Created by Julien SANCHEZ on 05/11/09.
  *  Copyright 2009 __MyCompanyName__. All rights reserved.
  *
  */
+
+#include "connexion.h"
 
