4ff289a8c592927241bc45e9af929727cc4f0476
sumbobyboys <sumbobyboys@e84e6728-c4a8-11de-8f3f-2f730f80caef>
2009-11-05 16:29:54 +0000

    Création de la phase de connexion

--- client/main.c
+++ client/main.c
@@ -1,8 +1,13 @@
+
 #include <stdio.h>
+
+#include "connexion.h"
+
 
 int main (int argc, const char * argv[]) {
    
-	
-	
+	identifiants* id;
+	id = newId();
+	inputConnexion(id);
     return 0;
 }
