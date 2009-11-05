8e4d6cbfe098bbbd6c6b94f3aef94283109dff17
sumbobyboys <sumbobyboys@e84e6728-c4a8-11de-8f3f-2f730f80caef>
2009-11-05 20:03:52 +0000

    création de l'input

--- trunk/client/main.c
+++ trunk/client/main.c
@@ -1,19 +1,29 @@
 
 #include <stdio.h>
 
-#include "connexion.h"
+#include "analyse.h"
 
 
 int main (int argc, const char * argv[]) {
 	
 	//Connexion : 
 	
-	identifiants* id;
-	id = newId();
-	inputConnexion(id);
+	/* identifiants* id;
+		id = newId();
+		inputConnexion(id); */
 	
+	//discution :
+	int bool = 1;
+	char * chaine;
 	
+	transmission* message;
+	message = newMessage();
 	
+	while(bool)
+	{
+		inputChaine(chaine);
+		analyse(chaine,message);
+	}
 	
     return 0;
 }
